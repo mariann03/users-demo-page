6ef8b1aa3c19eecfe885ffad374d8764cfa80457
mariano <mariann_03@outlook.com>
2020-03-18 16:15:34 -0300

    refactor(api-middlewares): update middlewares

--- app/utils/api/middlewares/withErrorHandler.js
+++ app/utils/api/middlewares/withErrorHandler.js
@@ -1,5 +1,6 @@
 export const NO_EMAIL_PASS = 'NO_EMAIL_PASS'
 export const ER_DUP_ENTRY = 'ER_DUP_ENTRY'
+export const ER_DUP_ENTRY_PROFILE = 'ER_DUP_ENTRY_PROFILE'
 export const EMAIL_PASSWORD_ERROR = 'EMAIL_PASSWORD_ERROR'
 export const NO_USER = 'NO_USER'
 export const NO_USER_PROFILE = 'NO_USER_PROFILE'
@@ -14,35 +15,36 @@ export default function withErrorHandler(handler) {
     } catch (error) {
       switch (error) {
         case NO_EMAIL_PASS:
-          res.status(400).json({ error: 'You must fill all fields' })
+          res.status(400).send('You must fill all fields')
           break
         case ER_DUP_ENTRY:
-          res.status(400).json({ error: 'The email is already registered' })
+          res.status(400).send('The email is already registered')
+          break
+        case ER_DUP_ENTRY_PROFILE:
+          res.status(400).send('The user profile is already created')
           break
         case EMAIL_PASSWORD_ERROR:
-          res.status(400).json({ error: 'Wrong email or password' })
+          res.status(400).send('Wrong email or password')
           break
         case NO_USER:
-          res.status(404).json({ error: 'User does not exists' })
+          res.status(404).send('User does not exists')
           break
         case NO_USER_PROFILE:
-          res.status(404).json({ error: 'User profile not exists' })
+          res.status(404).send('User profile not exists')
           break
         case TOKEN_ERROR:
-          res
-            .status(401)
-            .json({ error: 'The access token provided is expired, malformed, or invalid for other reasons' })
-          console.log(error)
+          res.status(401).send('The access token provided is expired, malformed, or invalid for other reasons')
+          console.error(error)
           break
         case BAD_QUERY:
-          res.status(501).json({ error: 'Query type is not supported' })
+          res.status(501).send('Query type is not supported')
           break
         case FORBIDDEN:
-          res.status(403).json({ error: "You don't have permission for this" })
+          res.status(403).send("You don't have permission for this")
           break
         default:
-          res.status(500).json({ error: 'Something went wrong' })
-          console.error('Something went wrong')
+          res.status(500).send('Something went wrong')
+          console.error(error)
       }
     }
   }
--- app/utils/api/middlewares/withUser.js
+++ app/utils/api/middlewares/withUser.js
@@ -1,11 +1,12 @@
 import { query } from '../../../lib/db'
 import withAutorization from './withAutorization'
-import withErrorHandler, { NO_USER } from './withErrorHandler'
 import { errorIf } from '../errors'
+
+import withErrorHandler, { NO_USER } from './withErrorHandler'
 
 function withUser(handler) {
   return async (req, res) => {
-    const [user] = await query`SELECT * FROM users WHERE id=${req.userId}`
+    const [user] = await query`SELECT id, email, role, created_at FROM users WHERE id=${req.userId}`
     errorIf(!user, NO_USER)
 
     req.user = user
